a3cec99d9c4f61f5c76bbd2d3dbfd867198a889b
ptolemycasio <you.likelihood@gmail.com>
2020-08-28 12:45:06 +0900

    入退出時の処理1.2 ユーザ名表示

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -1,10 +1,12 @@
 'use strict';
 
 // 入室メッセージをサーバに送信する
-// 入力されたユーザ名を取得する
-const userName = $('#userName').val();
-// 入室メッセージイベントを送信する
-socket.emit('entryMyselfEvent', userName);
+function enter() {
+    // 入力されたユーザ名を取得する
+    const userName = $('#userName').val();
+    // 入室メッセージイベントを送信する
+    socket.emit('entryMyselfEvent', userName);
+}
 
 // サーバから受信した入室メッセージを画面上に表示する
 socket.on('receiveEntryEvent', function (data) {
