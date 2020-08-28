d9cad2a9cdfe6b1e94b2cba78aa7b1df8980dcd8
dainateranishi <daina.teranishi@gmail.com>
2020-08-28 14:28:39 +0900

    追加要件1 空白での送信無効

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -2,15 +2,24 @@
 
 // 投稿メッセージをサーバに送信する
 function publish() {
+    //空白文字の正規表現 先頭から末尾まで空白文字
+    const regex = /^\s*$/;
     // ユーザ名を取得
     const userName = '';
     // 入力されたメッセージを取得
     const message = $('#message').val();
     console.log("textarea input : "  + message);
     //textareaを空にする
-    $('#message').val(" ")
+    $('#message').val(" ");
     // 投稿内容を送信
-    socket.emit("sendMessageEvent", message);
+
+    // socket.emit("sendMessageEvent", message);
+
+    if(regex.test(message)){
+        alert("文章を入力してください");
+    }else{
+        socket.emit("sendMessageEvent", message);
+    }
 
     return false;
 }
