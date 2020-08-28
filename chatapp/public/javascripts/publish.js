f4cb3b914d1b692a95d8eaf98676dd2f4eee50b4
dainateranishi <daina.teranishi@gmail.com>
2020-08-28 11:41:10 +0900

    投稿機能1　サーバーとクライアント間のテキストのやりとり

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -5,13 +5,15 @@ function publish() {
     // ユーザ名を取得
     const userName = '';
     // 入力されたメッセージを取得
-    const message = '';
+    const message = $('#message').val();
+    console.log("textarea input : "  + message);
     // 投稿内容を送信
+    socket.emit("sendMessageEvent", message);
 
     return false;
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('BroadcastEvent', function (data) {
+    $('#thread').prepend('<p>' + data +'</p>');
 });
--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -2,7 +2,14 @@
 
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
-    socket.on('', function (data) {
+    socket.on('sendMessageEvent', function (data) {
+        if(!data){
+            return;
+        }
+
+        console.log("クライアントの入力 :" + data);
+
+        io.sockets.emit("BroadcastEvent", data);
 
     });
 };
