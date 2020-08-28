bdf93bb361622e2cd21ff1ed2696e8af551b8acc
masal9pse <returnymgstokh@icloud.com>
2020-08-28 22:54:46 +0900

    ログイン処理＋チャット側に入力したユーザー名を表示

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -3,9 +3,13 @@
 // チャットルームに入室する
 function enter() {
     // 入力されたユーザ名を取得する
-    const userName = '';
+    // routes側で取得したデータを保存する？
+    const userName = $('#userName').val();
     // ユーザ名が未入力でないかチェックする
-
-
-    $('form').submit();
+    if (userName === "") {
+        alert('ユーザー名を入力してください');
+        return false;
+    } else {
+        $('form').submit();
+    }
 }
