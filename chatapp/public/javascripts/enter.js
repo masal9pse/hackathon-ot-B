90b3bb5a4814f6cd87164c60e7c39c9c0cc3d6dd
ptolemycasio <you.likelihood@gmail.com>
2020-08-28 12:57:09 +0900

    入退出時の処理1.3 修正

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -1,12 +1,10 @@
 'use strict';
 
 // 入室メッセージをサーバに送信する
-function enter() {
-    // 入力されたユーザ名を取得する
-    const userName = $('#userName').val();
-    // 入室メッセージイベントを送信する
-    socket.emit('entryMyselfEvent', userName);
-}
+// 入力されたユーザ名を取得する
+const userName = $('#userName').val();
+// 入室メッセージイベントを送信する
+socket.emit('entryMyselfEvent', userName);
 
 // サーバから受信した入室メッセージを画面上に表示する
 socket.on('receiveEntryEvent', function (data) {
