525a2c844862cde08540745f3790f280367829c0
masal9pse <returnymgstokh@icloud.com>
2020-08-29 14:17:32 +0900

    ターミナル上で日時取得の確認

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function (request, response, next) {
 // チャット画面の表示
 router.post('/room', function (request, response, next) {
     console.log('ユーザ名：' + request.body.userName);
+    console.log('現在日時：' + request.body.loginTime);
     // フロント側には正しく受け渡しできた。
     response.render('room', { userName: request.body.userName });
 });
