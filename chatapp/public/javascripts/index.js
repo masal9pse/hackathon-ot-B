8685b2cb797dd271f7c8e5537a5d680747c0e188
masal9pse <returnymgstokh@icloud.com>
2020-08-29 14:15:40 +0900

    マイクロ秒単位で現在日時を取得

--- chatapp/public/javascripts/index.js
+++ chatapp/public/javascripts/index.js
@@ -5,9 +5,12 @@ function enter() {
     // 入力されたユーザ名を取得する
     // routes側で取得したデータを保存する？
     const userName = $('#userName').val();
+    const loginTime = $('#loginTime').val($.now());
+
     // ユーザ名が未入力でないかチェックする
     if (userName === "") {
-        alert('ユーザー名を入力してください');
+        // alert('ユーザー名を入力してください');
+        alert($.now());
         return false;
     } else {
         $('form').submit();
