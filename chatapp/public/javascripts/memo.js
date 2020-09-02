f317fefb0fe3371753883bc5364e5b44338af452
konkon-h <konh.hnok@gmail.com>
2020-09-03 02:01:13 +0900

    メモ投稿

--- chatapp/public/javascripts/memo.js
+++ chatapp/public/javascripts/memo.js
@@ -3,12 +3,17 @@
 // メモを画面上に表示する
 function memo() {
     // ユーザ名を取得
-    const userName = '';
+    const userName = $("#userName").val();
     // 入力されたメッセージを取得
     const message = $('#message').val();
+    //入力日時を取得
+    let now = new Date($.now());
+    //textareaを空にする
+    $('#message').val(" ");
+
     // メモの内容を表示
     // $('#thread').text(message);
-    $('#thread').prepend('<p>' + message + '</p>');
+    $('#thread').prepend('<p>' + now.toLocaleString() + ":" + userName + '</p>' + '<p>' + message + '</p>');
 
     return false;
 }
