8ae05ec4929283bda1fa1371164124e248d9722a
masal9pse <returnymgstokh@icloud.com>
2020-09-05 14:19:47 +0900

    ログイン処理の途中経過をコミット

--- chatapp/sockets/log_in.js
+++ chatapp/sockets/log_in.js
@@ -1,28 +1,35 @@
-'use strict';
+"use strict";
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./database/usersdb.sqlite'); //データベースを新規に開く
 
 module.exports = function (socket, master) {
-    socket.on('logInAuthRequest', function (data) {
-        // データベースにユーザ名が登録されているか確認
-        
+    socket.on("logInAuthRequest", function (data) {
+        db.serialize(function () {
 
-        // パスワードが合っているか確認
+            // データベースにユーザ名が登録されているか確認
+            let username_sql = db.prepare("select count(*) from users where username=?");
+            let user_db_check = username_sql.run(data.user);
 
+            // パスワードが合っているか確認
+            if (user_db_check === true) {
+                let password_sql = db.prepare("select count(*) from users where password=?");
+                let password_db_check = password_sql.run(data.pass);
+            }
 
-        // ログイン日時を更新
-        
+            // ログイン日時を更新 => あとで
 
-        console.log(data.user + '：' + data.pass);
-        socket.emit('logInApproval', true);
+        });
+
+        console.log(data.user + "：" + data.pass);
+        socket.emit("logInApproval", true);
     });
 
-    socket.on('signUpAuthRequest', function (data) {
+    socket.on("signUpAuthRequest", function (data) {
         // ユーザ名が登録されていないか確認
 
-
         // ユーザ名，パスワード，ログイン日時をデータベースに挿入
-        
 
-        console.log(data.user + '：' + data.pass);
-        socket.emit('signUpApproval', true)
+        console.log(data.user + "：" + data.pass);
+        socket.emit("signUpApproval", true);
     });
 };
