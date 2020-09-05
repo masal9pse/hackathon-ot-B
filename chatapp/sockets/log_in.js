c665396a660c1c425936f62f2c6e4e42c53f3cb3
masal9pse <returnymgstokh@icloud.com>
2020-09-06 06:41:22 +0900

    サインアップ機能実装、ごっつあんゴール

--- chatapp/sockets/log_in.js
+++ chatapp/sockets/log_in.js
@@ -11,12 +11,21 @@ module.exports = function (socket, master) {
                 function (err, row) {
                     if (row) { // データベースにユーザ名が登録されているか
                         if (row.password === data.pass) { // パスワードが合っているか
-                            socket.emit("logInApproval", {approval: true, alert: "ログインできます"});
+                            socket.emit("logInApproval", {
+                                approval: true,
+                                alert: "ログインできます"
+                            });
                         } else {
-                            socket.emit("logInApproval", {approval: false, alert: "パスワードが間違っています"});
+                            socket.emit("logInApproval", {
+                                approval: false,
+                                alert: "パスワードが間違っています"
+                            });
                         }
                     } else {
-                        socket.emit("logInApproval", {approval: false, alert: "そのユーザー名は存在しません"});
+                        socket.emit("logInApproval", {
+                            approval: false,
+                            alert: "そのユーザー名は存在しません"
+                        });
                     }
                 }
             );
@@ -28,15 +37,21 @@ module.exports = function (socket, master) {
         const db = new sqlite3.Database('./database/usersdb.sqlite');
 
         db.serialize(function () {
-            db.get(`select username, password from users where username='${data.user}'`,
+            db.get(`select * from users where username='${data.user}'`,
                 function (err, row) {
                     if (row === undefined) { // データベースにユーザ名が登録されていないか
                         const stmt = db.prepare("insert into users values (?, ?, ?)");
                         stmt.run(data.user, data.pass, socket.id);
-                        stmt.finalize();
-                        socket.emit("logInApproval", {approval: true, alert: "登録可能です"});
+                        // stmt.finalize();
+                        socket.emit("signUpApproval", {
+                            approval: true,
+                            alert: "登録可能です"
+                        });
                     } else {
-                        socket.emit("logInApproval", {approval: false, alert: "そのユーザー名はすでに登録されています"});
+                        socket.emit("signUpApproval", {
+                            approval: false,
+                            alert: "そのユーザー名はすでに登録されています"
+                        });
                     }
                 }
             );
