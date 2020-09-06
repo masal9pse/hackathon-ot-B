c786268246891bd42bfa53b089095b07de17f145
ptolemycasio <you.likelihood@gmail.com>
2020-09-06 19:33:09 +0900

    sockets/publish.jsを少し整形

--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -18,11 +18,23 @@ module.exports = function (socket, io, master) {
         // console.log(io.sockets.clients());
         
         //他のクライアントには普通に送信
-        socket.broadcast.to(data.room).emit("receiveMessageEvent", {"num_message": num_message, "username": add_a_tag(data.username, num_message), "date": data.date, 
-        "msg": format(data.msg), "rp_button" : generate_reply(num_message), "rm_button": ""});
+        socket.broadcast.to(data.room).emit("receiveMessageEvent", {
+            "num_message": num_message,
+            "username": add_a_tag(data.username, num_message),
+            "date": data.date, 
+            "msg": format(data.msg),
+            "rp_button" : generate_reply(num_message),
+            "rm_button": ""
+        });
         //自分自身にはbタグをつけた内容を送信
-        socket.emit("receiveMessageEvent", {"num_message": num_message, "username": add_a_tag(data.username, num_message), "date": data.date, "msg": "<b>"+format(data.msg)+"</b>", 
-        "rp_button" : generate_reply(num_message), "rm_button": generate_remove(num_message)});
+        socket.emit("receiveMessageEvent", {
+            "num_message": num_message,
+            "username": add_a_tag(data.username, num_message),
+            "date": data.date,
+            "msg": "<b>"+format(data.msg)+"</b>", 
+            "rp_button" : generate_reply(num_message),
+            "rm_button": generate_remove(num_message)
+        });
 
         wait(wait_time, socket, io);　　　　//60秒間投稿禁止
 
@@ -44,13 +56,24 @@ module.exports = function (socket, io, master) {
         console.log(to_user);
 
         //自分自身への送信
-        socket.emit("receiveMessageEvent", {"num_message": num_message, "username": add_a_tag(data.username, num_message), "date": data.date, "msg": "<b>"+format(data.msg)+"</b>", 
-        "rp_button" : generate_reply(num_message), "rm_button": generate_remove(num_message)});
+        socket.emit("receiveMessageEvent", {
+            "num_message": num_message,
+            "username": add_a_tag(data.username, num_message),
+            "date": data.date,
+            "msg": "<b>"+format(data.msg)+"</b>", 
+            "rp_button" : generate_reply(num_message),
+            "rm_button": generate_remove(num_message)
+        });
 
         //特定のユーザに向けての送信
-        io.to(master[to_user].socketID).emit("receiveMessageEvent", 
-        {"num_message": num_message, "username": add_a_tag(data.username, num_message), "date": data.date, "msg": format(data.msg), "rp_button" : generate_reply(num_message), "rm_button": ""});
-
+        io.to(master[to_user].socketID).emit("receiveMessageEvent", {
+            "num_message": num_message,
+            "username": add_a_tag(data.username, num_message),
+            "date": data.date,
+            "msg": format(data.msg),
+            "rp_button" : generate_reply(num_message),
+            "rm_button": ""
+        });
 
         wait(wait_time, socket, io);　　　　//60秒間投稿禁止
     });
@@ -64,11 +87,25 @@ module.exports = function (socket, io, master) {
         let to_reply = String(data.reply).replace("@", "").replace("\n", "");
 
         //他のクライアントには普通に送信
-        socket.broadcast.to(data.room).emit("receiveReplyMessage", {"num_message": num_message, "reply": to_reply, "username": add_a_tag(data.username, num_message), "date": data.date, 
-        "msg": format(data.msg), "rp_button" : generate_reply(num_message), "rm_button": ""});
+        socket.broadcast.to(data.room).emit("receiveReplyMessage", {
+            "num_message": num_message,
+            "reply": to_reply,
+            "username": add_a_tag(data.username, num_message),
+            "date": data.date, 
+            "msg": format(data.msg),
+            "rp_button" : generate_reply(num_message),
+            "rm_button": ""
+        });
         //自分自身にはbタグをつけた内容を送信
-        socket.emit("receiveReplyMessage", {"num_message": num_message, "reply": to_reply, "username": add_a_tag(data.username, num_message), "date": data.date, "msg": "<b>"+format(data.msg)+"</b>", 
-        "rp_button" : generate_reply(num_message), "rm_button": generate_remove(num_message)});
+        socket.emit("receiveReplyMessage", {
+            "num_message": num_message,
+            "reply": to_reply,
+            "username": add_a_tag(data.username, num_message),
+            "date": data.date,
+            "msg": "<b>"+format(data.msg)+"</b>", 
+            "rp_button" : generate_reply(num_message),
+            "rm_button": generate_remove(num_message)
+        });
 
         wait(wait_time, socket, io);　　　　//60秒間投稿禁止
 
