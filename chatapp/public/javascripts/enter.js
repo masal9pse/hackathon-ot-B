7d8852f2fb429f565996fe1c773d792e7db189e4
dainateranishi <daina.teranishi@gmail.com>
2020-09-08 17:40:18 +0900

    DMの修正

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -30,6 +30,6 @@ socket.on('receiveWelcomeEvent', function (data) {
 socket.on('receiveEntryUserList', function (userList) {
     $('#member-list').empty();
     userList.forEach(member => {
-        $('#member-list').prepend(`<li>${member}</li>`);
+        $('#member-list').prepend(`<li><a href='#' onclick='OnUsernameClick(this, "tl");' id=${member}>${member}</a></li>`);
     });
 });
