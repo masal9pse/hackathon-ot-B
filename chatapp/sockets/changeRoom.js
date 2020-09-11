9f5895ed2120f1235d57cbde46dad95d3d71506f
ptolemycasio <you.likelihood@gmail.com>
2020-09-12 03:49:32 +0900

    ルーム遷移修正

--- chatapp/sockets/changeRoom.js
+++ chatapp/sockets/changeRoom.js
@@ -5,11 +5,14 @@ module.exports = function(socket, io, master, helper, history) {
     socket.on('changeRoomEvent', function(data) {
         console.log(`changeRoomしたユーザ名：${data.user_name}`);
 
+        // 他クライアントが受信する入室表示イベントを送信する
+        // 入室したルームに既に自分のアカウントがいる場合は送信しない
+        if (!Object.values(master[data.user_name].socketID).includes(data.next_room)) {
+            socket.to(data.next_room).emit('receiveEntryEvent', data.user_name);
+        }
+
         // Managerで管理しているユーザのSocketIdに対応するルーム名を更新
         master[data.user_name].socketID[socket.id] = data.next_room;
-
-        // 遷移前のroomから退室する
-        socket.leave(data.pre_room);
 
         // 他クライアントが受信する退室表示イベントを送信する
         // 退室したルームにまだ自分のアカウントがいる場合は送信しない
@@ -17,14 +20,11 @@ module.exports = function(socket, io, master, helper, history) {
             socket.to(data.pre_room).emit('receiveExitEvent', data.user_name);
         }
 
+        // 遷移前のroomから退室する
+        socket.leave(data.pre_room);
+
         // 遷移先のroomに入室する
         socket.join(data.next_room);
-
-        // 他クライアントが受信する入室表示イベントを送信する
-        // 入室したルームに既に自分のアカウントがいる場合は送信しない
-        if (!Object.values(master[data.user_name].socketID).includes(data.next_room)) {
-            socket.to(data.next_room).emit('receiveEntryEvent', data.user_name);
-        }
 
         // ルームに入室中のユーザーを送信する
         socket.to(data.pre_room).emit('RoomEntryUserList',
